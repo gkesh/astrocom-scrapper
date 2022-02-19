4e94d68be466da3b39df762182c8f96f4a979a60
gkesh <gkeshthapa@gmail.com>
2022-02-19 22:40:09 +0545

    Documentation on crawlers

--- engine/crawler.py
+++ engine/crawler.py
@@ -1,3 +1,14 @@
+"""
+Crawlers go through the page and find the image
+and create an array of links to the image.
+
+Currently, there are two crawlers in written,
+one is for kissmanga and one is a custom crawler
+written to get the kaiju8 manga from the kaiju8
+site.
+
+@author gkesh
+"""
 kissmanga_crawler = lambda soup : [img["src"] for img in soup.find("div", {"id" : "centerDivVideo"}).findAll("img", recursive = False)]
 kaiju_crawler = lambda soup : [img["src"] for img in soup.find("div", {"class": "entry-content"}).findAll("img", recursive = True)]
 
